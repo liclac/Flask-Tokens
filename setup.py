84156d7b3222e45094dd794c18fc2aa2177be17f
Johannes Ekberg <uppfinnarn@gmail.com>
2014-10-24 00:55:06 +0200

    Yay, packaging

---
+++ setup.py
@@ -0,0 +1,36 @@
+"""
+Flask-Tokens
+-------------
+
+Flask-Tokens handles token-based authentication for Flask applications.
+"""
+from setuptools import setup
+
+
+setup(
+    name='Flask-Tokens',
+    version='0.1',
+    url='http://github.com/uppfinnarn/Flask-Tokens',
+    license='MIT',
+    author='Johannes Ekberg',
+    author_email='uppfinnarn@example.com',
+    description='Token authentication for your Flask applications',
+    long_description=__doc__,
+    packages=['flask_tokens'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask',
+        'pyjwt'
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
